6ff88ff0d770fa18f5c66418494317cf3d27b13b
Dom Davis <dom@domdavis.com>
2016-12-03 14:00:39 +0000

    Add solutions for day 3

--- 2016/day3/day3.go
+++ 2016/day3/day3.go
@@ -1,9 +1,78 @@
 package day3
 
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var matcher = regexp.MustCompile(`\s*([\d]+)\s+([\d]+)\s+([\d]+)`)
+
 func Solution() string {
-	return "Not yet solved"
+	return fmt.Sprintf("Part 1: %s, Part 2: %s",
+		strconv.Itoa(horizontal(data)),
+		strconv.Itoa(vertical(data)))
 }
 
-func Triangle(lengths string) bool {
-	return true
+func horizontal(input string) int {
+	return count(strings.Split(input, "\n"))
+}
+
+func vertical(input string) int {
+	sides := make([]string, 3)
+	total := 0
+	c := 0
+	for _, line := range strings.Split(input, "\n") {
+
+		m := matcher.FindStringSubmatch(line)
+
+		if len(m) != 4 {
+			panic(fmt.Sprintf("Invalid input: '%s'", line))
+		}
+
+		for i := range sides {
+			sides[i] = fmt.Sprintf("%s %s", sides[i], m[i+1])
+		}
+
+		c++
+
+		if c == 3 {
+			total += count(sides)
+			sides = make([]string, 3)
+			c = 0
+		}
+
+	}
+
+	return total
+}
+
+func count(sides []string) int {
+	count := 0
+	for _, i := range sides {
+		if triangle(i) {
+			count++
+		}
+	}
+
+	return count
+}
+
+func triangle(input string) bool {
+	sides := make([]int, 3)
+	m := matcher.FindStringSubmatch(input)
+
+	if len(m) != 4 {
+		panic(fmt.Sprintf("Invalid triangle input: '%s'", input))
+	}
+
+	for i := range sides {
+		sides[i], _ = strconv.Atoi(m[i+1])
+	}
+
+	sort.Ints(sides)
+
+	return sides[0]+sides[1] > sides[2]
 }
--- 2016/day3/day3_test.go
+++ 2016/day3/day3_test.go
@@ -3,10 +3,16 @@ package day3
 import "fmt"
 
 func ExampleSolution_1() {
-	fmt.Println(Triangle("3 4 5"))
-	fmt.Println(Triangle("5 10 25"))
+	fmt.Println(triangle("3 4 5"))
+	fmt.Println(triangle("5 10 25"))
 
 	// Output:
 	// true
 	// false
+}
+
+func ExampleSolution_2() {
+	fmt.Println(horizontal(data))
+
+	// Output: 1032
 }
