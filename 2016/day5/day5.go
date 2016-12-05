864ce94d09e4cb63dc98fc15dca8f9440597d746
Dom Davis <dom@domdavis.com>
2016-12-05 11:47:12 +0000

    Add brute force solution for day 5 part 1

--- 2016/day5/day5.go
+++ 2016/day5/day5.go
@@ -1,9 +1,36 @@
 package day5
 
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+)
+
 func Solution() string {
 	return decode("abbhdwsy")
 }
 
+const prefix = "00000"
+
 func decode(input string) string {
-	return "Not yet solved"
+	suffix := int32(0)
+	code := ""
+
+	for i := 0; i < 8; i++ {
+		searching := true
+
+		for searching {
+			data := []byte(fmt.Sprintf("%s%d", input, suffix))
+			r := fmt.Sprintf("%x", md5.Sum(data))
+			suffix++
+
+			if strings.HasPrefix(r, prefix) {
+				code = fmt.Sprintf("%s%s", code, string(r[5]))
+				searching = false
+			}
+		}
+
+	}
+
+	return string(code)
 }
