e782236bee81800676d2a04fa14ae8b086fe42d8
Dom Davis <dom@domdavis.com>
2016-12-08 13:03:19 +0000

    Add day 8 part 2 solution

--- 2016/day8/day8.go
+++ 2016/day8/day8.go
@@ -7,12 +7,8 @@ import (
 )
 
 func Solution() string {
-	return fmt.Sprintf("Part 1: %d, Part 2: %s",
-		part1(data), "Not yet done")
-}
-
-func part1(input string) int {
 	s := display.New(50, 6)
-	s.BulkTransform(input)
-	return s.Count()
+	s.BulkTransform(data)
+	return fmt.Sprintf("Part 1: %d, Part 2:\n%s",
+		s.Count(), s)
 }
