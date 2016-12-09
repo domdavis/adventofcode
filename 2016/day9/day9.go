859c5921555ff1d21fa446eaabab8feaec1a8f25
Dom Davis <dom@domdavis.com>
2016-12-09 19:55:04 +0000

    Add day 9 part 1 solution

--- 2016/day9/day9.go
+++ 2016/day9/day9.go
@@ -1,12 +1,45 @@
 package day9
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var marker = regexp.MustCompile(`\((\d+)x(\d+)\)`)
 
 func Solution() string {
 	return fmt.Sprintf("Part 1: %d, Part 2: %s",
-		len(decode(data)), decode(data))
+		len(decode(data)), "Not yet done")
 }
 
 func decode(input string) string {
-	return input
+	output := ""
+	for i := 0; i < len(input); i++ {
+		c := string(input[i])
+
+		if c != "(" {
+			output += c
+		} else {
+			length, next, repeat := getMarker(input[i:])
+			i += length
+			s := input[i : i+next]
+
+			for ; repeat > 0; repeat-- {
+				output += s
+			}
+
+			i += next - 1
+		}
+	}
+
+	return output
+}
+
+func getMarker(input string) (int, int, int) {
+	match := marker.FindStringSubmatch(input)
+	next, _ := strconv.Atoi(match[1])
+	repeat, _ := strconv.Atoi(match[2])
+
+	return len(match[0]), next, repeat
 }
